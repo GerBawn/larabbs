ecf4b3596e73633c90ed58a774495e6516bc71aa
gerbawn <gerbawn@gmail.com>
2018-07-09 17:27:26 +0800

    create topics table

---
+++ database/migrations/2018_07_09_171909_create_topics_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->index();
+            $table->text('body');
+            $table->integer('user_id')->index()->unsigned();
+            $table->integer('category_id')->index()->unsigned();
+            $table->integer('reply_count')->index()->unsigned()->default(0);
+            $table->integer('view_count')->index()->unsigned()->default(0);
+            $table->integer('last_reply_user_id')->index()->unsigned()->default(0);
+            $table->integer('order')->default(0);
+            $table->text('excerpt');
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
