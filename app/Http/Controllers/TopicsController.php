6791313470461a145089a86d0f2188717476d51f
GerBawn <gerbawn@gmail.com>
2018-07-10 11:01:43 +0800

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Models\Topic;
@@ -42,5 +43,26 @@ class TopicsController extends Controller
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        $data = [
+            'success' => false,
+            'msg' => '上传失败！',
+            'file_path' => '',
+        ];
+
+        if ($file = $request->upload_file) {
+            $result = $uploader->save($file, 'topics', Auth::id(), 1024);
+
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功！';
+                $data['success'] = true;
+            }
+        }
+
+        return $data;
     }
 }
