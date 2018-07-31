d0a9da184445f9803d062fb843228a16d285928b
GerBawn <gerbawn@gmail.com>
2018-07-31 19:54:25 +0800

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Request $request, Category $category, Topic $topic, User $user)
+    public function show(Request $request, Category $category, Topic $topic, User $user, Link $link)
     {
         $topics = $topic->withOrder('order', $request->order)
             ->where('category_id', $category->id)->paginate(20);
 
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
 
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Link;
 use App\Models\Reply;
+use App\Observers\LinkObserver;
 use App\Observers\ReplyObserver;
 use App\Observers\TopicObserver;
 use App\Models\Topic;
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setLocale('zh');
         Topic::observe(TopicObserver::class);
         Reply::observe(ReplyObserver::class);
+        Link::observe(LinkObserver::class);
     }
 
     /**
