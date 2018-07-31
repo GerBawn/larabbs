027afe01c630921e9440fabad5362a5e1f9f3bec
GerBawn <gerbawn@gmail.com>
2018-07-31 14:46:50 +0800

    Admin can manage content

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -17,3 +17,31 @@ if (!function_exists('makeExcerpt')) {
         return str_limit($excerpt, $length);
     }
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+if (!function_exists('model_link')) {
+    function model_link($title, $model, $prefix = '')
+    {
+        $modelName = model_plural_name($model);
+        $prefix = $prefix ? "/$prefix/" : '/';
+
+        $url = config('app.url') . $prefix . $modelName . '/' . $model->id;
+
+        return '<a href="' . $url . '" target="_blank">' . $title . '</a>';
+    }
+}
+
+if (!function_exists('model_plural_name')) {
+    function model_plural_name($model)
+    {
+        $fullClassName = get_class($model);
+        $className = class_basename($fullClassName);
+        $snakeCaseName = snake_case($className);
+
+        return str_plural($snakeCaseName);
+    }
+}
